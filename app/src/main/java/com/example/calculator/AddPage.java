ad72b04ac66bd09a70eeda0565dc432ac64a3810
fabin-sunny <fabinsunny@gmail.com>
2024-06-27 23:39:00 +0530

    project created

---
+++ app/src/main/java/com/example/calculator/AddPage.java
@@ -0,0 +1,52 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddPage extends AppCompatActivity {
+    EditText ed1,ed2;
+    AppCompatButton b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_page);
+        ed1=(EditText) findViewById(R.id.onenum);
+        ed2=(EditText) findViewById(R.id.twonum);
+        b1=(AppCompatButton) findViewById(R.id.add);
+        b2=(AppCompatButton) findViewById(R.id.gotomenu);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    int n1 = Integer.parseInt(ed1.getText().toString());
+                    int n2 = Integer.parseInt(ed2.getText().toString());
+                    int result = n1 + n2;
+                    Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(getApplicationContext(),"Enter the numbers", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton b1,b2,b3,b4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b1=(AppCompatButton) findViewById(R.id.addbut);
+        b2=(AppCompatButton) findViewById(R.id.sub);
+        b3=(AppCompatButton) findViewById(R.id.mul);
+        b4=(AppCompatButton) findViewById(R.id.div);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),AddPage.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),SubPage.class);
+                startActivity(i);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MulPage.class);
+                startActivity(i);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),DivPage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
